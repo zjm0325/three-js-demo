06937c193bfb3d6a5c9d8672e724f8b395f8616d
zjm0325 <18916204470@163.com>
2021-02-14 14:27:46 +0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DemoOneComponent } from './demo-one/demo-one.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, children:
+      [
+        { path: '', redirectTo: 'demo-one', pathMatch: 'full' },
+        { path: 'demo-one', component: DemoOneComponent }
+      ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoOneComponent } from './demo-one/demo-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoOneComponent
   ],
   imports: [
     BrowserModule,
